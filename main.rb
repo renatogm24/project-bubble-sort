08d6e17fc726583a92b0946499f84dcb2dbb8ac1
Renato <renato.garay@pucp.pe>
2020-10-21 16:42:38 +0000

    More efficient way of bubble sort

--- main.rb
+++ main.rb
@@ -1,28 +1,25 @@
-def bubble_sort(list)
-  result = []
-  count = 0
-  ordered_items = 0
-  change = false
-  list.length.times do
-    list.each_with_index do |stock_1, idx|
-      list.each_with_index do |stock_2, idy|      
-        if stock_2 < stock_1  && idy-idx == 1
-          change = true
-          list[idx] = stock_2
-          list[idy] = stock_1
-          next
-        elsif idy-idx == 1
-          next
-        end
-      count += 1  
+def bubble_sort(array)
+  array_length = array.size
+  return array if array_length <= 1
+  #count = 0
+  loop do
+    # we need to create a variable that will be checked so that we don't run into an infinite loop scenario.
+    swapped = false
+
+    # subtract one because Ruby arrays are zero-index based
+    (array_length-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
       end
+      #puts "iteration: #{i}"
+      #count += 1
     end
-    change = false
-    result.push(list.last)
-    list.pop    
-  end 
-  p count
-  return result.reverse!
+
+    break if not swapped
+  end
+  #p count
+  array
 end
 
 p bubble_sort([4,3,78,2,0,2])
---
+++ optional_solution.rb
@@ -0,0 +1,28 @@
+def bubble_sort(list)
+  result = []
+  count = 0
+  ordered_items = 0
+  change = false
+  (list.length).times do
+    list.each_with_index do |stock_1, idx|
+      list.each_with_index do |stock_2, idy|      
+        if stock_2 < stock_1  && idy-idx == 1
+          change = true
+          list[idx] = stock_2
+          list[idy] = stock_1
+          next
+        elsif idy-idx == 1
+          next
+        end
+      count += 1  
+      end
+    end
+    change = false
+    result.push(list.last)
+    list.pop    
+  end 
+  p count
+  return result.reverse!
+end
+
+p bubble_sort([4,3,78,2,0,2])
